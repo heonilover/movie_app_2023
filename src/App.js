39b7afe6b4ee3ece8ffeac36e862ad1b679d6b91
heonilover <perpor2@gmail.com>
2023-06-22 11:01:58 +0900

    1254636

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+function Food({ fav }){
+  return <h1>I like {fav}</h1>
+}
+
 function App() {
-    return <div>Hello!!!!!</div>
-  ;
+  return(
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+    </div>
+  );
 }
 
 export default App;
