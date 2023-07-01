fa004c0090149b7929ccb2f04c0b3c22bb133c06
heonilover <pespor2@gmail.com>
2023-07-01 23:12:30 +0900

    sadfasdfsadf

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function Food({ fav }){
-  return <h1>I like {fav}</h1>
-}
+class App extends React.Component{
+  state={
+    isLoading: true,
+    movies: [],
+  };
 
-function App() {
-  return(
-    <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chokumi" />
-    </div>
-  );
+  getMovies=async()=>{
+    const{
+      data:{
+        data:{movies},
+      },
+    }=await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({movies, isLoading: false});
+  }
+
+  componentDidMount(){
+    // 영화 데이터 로딩!
+    this.getMovies();
+  }
+
+  render(){
+    const{isLoading, movies}=this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movie">
+            {movies.map(movie=>(
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              /> 
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
